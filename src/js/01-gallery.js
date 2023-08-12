945af7f6f41fb2d1a3b4e3420a241b3b48681147
Andrii111111111 <129265573+Andrii111111111@users.noreply.github.com>
2023-08-12 12:22:33 +0200

    done task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,43 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from "./gallery-items";
+import SimpleLightbox from "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from "simplelightbox/dist/simple-lightbox.esm";
 // Change code below this line
 
-console.log(galleryItems);
+
+const list = document.querySelector(".gallery")
+
+function writeMarkup(arr) {
+   return arr.map(({ preview, original, description }) =>
+  `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</li> `).join('')
+}
+
+
+list.insertAdjacentHTML("afterbegin", writeMarkup(galleryItems))
+
+list.addEventListener("click", getBigImg)
+function getBigImg(evt) {
+
+  evt.preventDefault()
+
+   if (!evt.target.classList.contains('gallery__image') ) {
+     return;
+   }
+    var lightbox = new SimpleLightbox('.gallery a', { 
+        captionsData: 'alt',
+        captionPosition: 'bottom',
+        captionDelay: 250,
+  });
+  
+}
+const li = document.querySelector('.gallery__item')
+li.style.listStyle = "none"
+
