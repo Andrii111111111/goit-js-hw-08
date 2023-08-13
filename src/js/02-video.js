512000f9dde814ba56a89b1ff48147f9ae4e6456
Andrii111111111 <129265573+Andrii111111111@users.noreply.github.com>
2023-08-13 07:53:00 +0200

    1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
+const onPlay = function(data) {
+    // data is an object containing properties specific to that event
+    const getTime = data.seconds;
+    localStorage.setItem("videoplayer-current-time", getTime);
+   
+    const backTime = localStorage.getItem("videoplayer-current-time")
+    console.log(backTime)
+    player.setCurrentTime(backTime).then(function(getTime) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
+};
+
+player.on('timeupdate', onPlay);
+
 
