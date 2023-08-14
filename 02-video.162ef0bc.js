646d0bfb4338ec3dd6ecb2edb4a4e331cf42e1c3
Andrii111111111 <129265573+Andrii111111111@users.noreply.github.com>
2023-08-14 06:31:43 +0000

    Deploying to gh-pages from @ Andrii111111111/goit-js-hw-08@2ee869e201269f57ad56e43186f531c64c7e1d4a 🚀

--- 02-video.162ef0bc.js
+++
@@ -1,2 +0,0 @@
-player.on("timeupdate",(function(e){const t=e.seconds;localStorage.setItem("videoplayer-current-time",t);const n=localStorage.getItem("videoplayer-current-time");console.log(n),player.setCurrentTime(n).then((function(e){})).catch((function(e){e.name}))}));
-//# sourceMappingURL=02-video.162ef0bc.js.map
--- 02-video.cfae157d.js
+++
@@ -1,2 +0,0 @@
-player.on("timeupdate",(function(e){var t=e.seconds;localStorage.setItem("videoplayer-current-time",t);var n=localStorage.getItem("videoplayer-current-time");console.log(n),player.setCurrentTime(n).then((function(e){})).catch((function(e){e.name}))}));
-//# sourceMappingURL=02-video.cfae157d.js.map
