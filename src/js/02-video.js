5d04fdb2bd6c7e62255d54086d5412e8aa4b716e
Andrii111111111 <129265573+Andrii111111111@users.noreply.github.com>
2023-08-15 12:30:37 +0200

    done task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,28 +4,13 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 const LOCAL_KEY = 'videoplayer-current-time';
-const throttl = new throttle();
-// let f1000;
 // const throttle = require('lodash.throttle');
-// throttle(player, 1000);
-// player.on('timeupdate', function ({ seconds }) {
-//   localStorage.setItem(LOCAL_KEY, seconds);
-// });
 
-throttl(() => {
-  player.on('timeupdate', function ({ seconds }) {
+player.on(
+  'timeupdate',
+  throttle(function ({ seconds }) {
     localStorage.setItem(LOCAL_KEY, seconds);
-  });
-}, 3000);
+  }, 1000)
+);
 
-// let sss = throttle(player.on('timeupdate'), 1000);
-// let f1000 = throttle( function ({ seconds }), 1000)
-// player.on('timeupdate', function ({ seconds }) {
-//   localStorage.setItem(LOCAL_KEY, seconds);
-//   //   _.throttle(() => {
-//   //     localStorage.setItem(LOCAL_KEY, seconds);
-//   //   }, 1000);
-// });
 player.setCurrentTime(localStorage.getItem(LOCAL_KEY) || 0);
-
-console.log(throttle);
