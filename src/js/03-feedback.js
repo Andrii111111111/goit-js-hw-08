c10a98d8e1908f9e0fc67e670ca8fea40c3cc0cf
Andrii111111111 <129265573+Andrii111111111@users.noreply.github.com>
2023-08-16 10:37:40 +0200

    1

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,24 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const email = form.elements.email.value;
+const message = form.elements.message.value;
+const LOCAL_KEY = 'feedback-form-state';
+console.log(email);
+form.addEventListener('input', getData);
+
+function getData(evt) {
+  console.log(form.elements.email.value);
+  console.log(form.elements.message.value);
+  //   localStorage.setItem(LOCAL_KEY, evt.target.value);
+  const data = { email, message };
+  localStorage.setItem(LOCAL_KEY, data);
+  //   localStorage.setItem(LOCAL_KEY, form.elements.message.value);
+}
+
+document.addEventListener('DOMContentLoaded', backContent);
+
+function backContent(evt) {
+  evt.target.value = localStorage.getItem(LOCAL_KEY);
+  console.log(localStorage.getItem(LOCAL_KEY));
+}
