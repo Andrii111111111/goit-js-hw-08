ba2c47ac38e6b6630df15edf947536be16ba1ffc
Andrii111111111 <129265573+Andrii111111111@users.noreply.github.com>
2023-08-18 05:58:53 +0200

    fix

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,15 +1,16 @@
 // Add imports above this line
-import { galleryItems } from "./gallery-items";
-import SimpleLightbox from "simplelightbox/dist/simple-lightbox.min.css";
-import SimpleLightbox from "simplelightbox/dist/simple-lightbox.esm";
+import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox/dist/simple-lightbox.min.css';
+import SimpleLightbox from 'simplelightbox/dist/simple-lightbox.esm';
 // Change code below this line
 
-
-const list = document.querySelector(".gallery")
+const list = document.querySelector('.gallery');
 
 function writeMarkup(arr) {
-   return arr.map(({ preview, original, description }) =>
-  `<li class="gallery__item">
+  return arr
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
   <a class="gallery__link" href="${original}">
     <img
       class="gallery__image"
@@ -17,27 +18,18 @@ function writeMarkup(arr) {
       alt="${description}"
     />
   </a>
-</li> `).join('')
+</li> `
+    )
+    .join('');
 }
 
+list.insertAdjacentHTML('afterbegin', writeMarkup(galleryItems));
 
-list.insertAdjacentHTML("afterbegin", writeMarkup(galleryItems))
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
 
-list.addEventListener("click", getBigImg)
-function getBigImg(evt) {
-
-  evt.preventDefault()
-
-   if (!evt.target.classList.contains('gallery__image') ) {
-     return;
-   }
-    var lightbox = new SimpleLightbox('.gallery a', { 
-        captionsData: 'alt',
-        captionPosition: 'bottom',
-        captionDelay: 250,
-  });
-  
-}
-const li = document.querySelector('.gallery__item')
-li.style.listStyle = "none"
-
+const li = document.querySelector('.gallery__item');
+li.style.listStyle = 'none';
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,7 +2,7 @@ import throttle from 'lodash.throttle';
 
 const form = document.querySelector('.feedback-form');
 const subBtn = document.querySelector('button');
-const LOCAL_KEY = 'feedback-form-state';
+const LS_KEY = 'feedback-form-state';
 
 form.addEventListener('input', throttle(getData, 500));
 function getData(evt) {
@@ -10,19 +10,17 @@ function getData(evt) {
     email: form.elements.email.value,
     message: form.elements.message.value,
   };
-  localStorage.setItem(LOCAL_KEY, JSON.stringify(data));
+  localStorage.setItem(LS_KEY, JSON.stringify(data));
 }
 
 document.addEventListener('DOMContentLoaded', backContent);
 function backContent(evt) {
-  if (localStorage.getItem(LOCAL_KEY) === null) {
+  if (localStorage.getItem(LS_KEY) === null) {
     return;
   } else {
-    form.elements.email.value = JSON.parse(
-      localStorage.getItem(LOCAL_KEY)
-    ).email;
+    form.elements.email.value = JSON.parse(localStorage.getItem(LS_KEY)).email;
     form.elements.message.value = JSON.parse(
-      localStorage.getItem(LOCAL_KEY)
+      localStorage.getItem(LS_KEY)
     ).message;
   }
 }
@@ -30,8 +28,8 @@ function backContent(evt) {
 subBtn.addEventListener('click', sendData);
 function sendData(evt) {
   evt.preventDefault();
-  console.log(JSON.parse(localStorage.getItem(LOCAL_KEY)));
-  localStorage.clear(LOCAL_KEY);
+  console.log(JSON.parse(localStorage.getItem(LS_KEY)));
+  localStorage.removeItem(LS_KEY);
   form.elements.email.value = '';
   form.elements.message.value = '';
 }
